47f8fc7a7de59665529ab29ef7a7d6addd51cda4
SWEEPC2 <carterdbroom@gmail.com>
2024-04-06 21:31:48 -0400

    Implemented the code Chris gave us so that it works with the game. Still no sound quite yet.

---
+++ src/Sounds.elm
@@ -0,0 +1,38 @@
+module Sounds exposing (..)
+import Ports exposing (..)
+
+cSound : Sound
+cSound = { id = "C", url = ""}
+
+cSharpSound : Sound
+cSharpSound = { id = "CSharp", url = ""}
+
+dSound : Sound
+dSound = { id = "D", url = ""}
+
+dSharpSound : Sound
+dSharpSound = { id = "DSharp", url = ""}
+
+eSound : Sound
+eSound = { id = "E", url = ""}
+
+fSound : Sound
+fSound = { id = "F", url = ""}
+
+fSharpSound : Sound
+fSharpSound = { id = "FSharp", url = ""}
+
+gSound : Sound
+gSound = { id = "G", url = ""}
+
+gSharpSound : Sound
+gSharpSound = { id = "GSharp", url = ""}
+
+aSound : Sound
+aSound = { id = "A", url = ""}
+
+aSharpSound : Sound
+aSharpSound = { id = "ASharp", url = ""}
+
+bSound : Sound
+bSound = { id = "B", url = ""}
