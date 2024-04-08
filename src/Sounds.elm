4de6ec19472ccc6224c77582ec8bb20ca2cdb052
SWEEPC2 <carterdbroom@gmail.com>
2024-04-07 22:59:40 -0400

    Added the final audio to the game.

--- src/Sounds.elm
+++ src/Sounds.elm
@@ -2,37 +2,37 @@ module Sounds exposing (..)
 import Ports exposing (..)
 
 cSound : Sound
-cSound = { id = "C", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteC.mp3?raw=true"}
+cSound = { id = "C", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/c.mp3?raw=true"}
 
 cSharpSound : Sound
-cSharpSound = { id = "CSharp", url = ""}
+cSharpSound = { id = "CSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/cSharp.mp3?raw=true"}
 
 dSound : Sound
-dSound = { id = "D", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteD.mp3?raw=true"}
+dSound = { id = "D", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/d.mp3?raw=true"}
 
 dSharpSound : Sound
-dSharpSound = { id = "DSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteDSharp.mp3?raw=true"}
+dSharpSound = { id = "DSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/dSharp.mp3?raw=true"}
 
 eSound : Sound
-eSound = { id = "E", url = ""}
+eSound = { id = "E", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/e.mp3?raw=true"}
 
 fSound : Sound
-fSound = { id = "F", url = ""}
+fSound = { id = "F", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/f.mp3?raw=true"}
 
 fSharpSound : Sound
-fSharpSound = { id = "FSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteFSharp.mp3?raw=true"}
+fSharpSound = { id = "FSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/fSharp.mp3?raw=true"}
 
 gSound : Sound
-gSound = { id = "G", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteG.mp3?raw=true"}
+gSound = { id = "G", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/g.mp3?raw=true"}
 
 gSharpSound : Sound
-gSharpSound = { id = "GSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/guitarNoteGSharp.mp3?raw=true"}
+gSharpSound = { id = "GSharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/gSharp.mp3?raw=true"}
 
 aSound : Sound
-aSound = { id = "A", url = ""}
+aSound = { id = "A", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/a.mp3?raw=true"}
 
 aSharpSound : Sound
-aSharpSound = { id = "ASharp", url = ""}
+aSharpSound = { id = "ASharp", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/aSharp.mp3?raw=true"}
 
 bSound : Sound
-bSound = { id = "B", url = ""}
+bSound = { id = "B", url = "https://github.com/carterdbroom/timeless-tunes/blob/main/src/Sounds/b.mp3?raw=true"}
